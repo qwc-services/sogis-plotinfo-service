5de3caee79b2d6f6882567f2e67666f8f02f48c1
Sandro Mani <manisandro@gmail.com>
2026-07-02 12:58:30 +0200

    Update to qwc-services-core==1.6.0, use qwc_services_core.api.Api

--- src/server.py
+++ src/server.py
@@ -17,6 +17,7 @@ from qwc_services_core.runtime_config import RuntimeConfig
 
 # Flask application
 app = Flask(__name__)
+app.config['RESTX_NO_DEFAULT_ROOT_RULE'] = True
 app_nocache(app)
 api = Api(app, version='1.0', title='PlotInfo service API',
           description="""API for SO!MAP PlotInfo service.
